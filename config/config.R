cf3a3d86a6a4a7e33e5f4c9b07aaa420934e0877
philipshirk <philip.shirk@gmail.com>
2022-06-03 07:30:29 -0400

    Production run - 2022-06-02 - set 2 - aggregate BA.1.1 & include BA.2.12.1 (plus BA.4 & BA.5 in run1 & run2)

--- config/config.R
+++ config/config.R
@@ -59,7 +59,7 @@ custom_lineage_names <- c("BA.1+")
 # Set voc's for Run1
 voc1 = c(# "AY.1", "AY.2",
          # "BA.1",
-         "BA.1.1",
+         # "BA.1.1",
          "BA.2",
          "BA.2.12.1",
          'BA.4',
@@ -87,7 +87,7 @@ voc2_manual = c(NA)
 # (this will not have any effect if "voc2_manual" is used)
 voc2_additional = c(#"AY.1", "AY.2",
                     "BA.1",
-                    "BA.1.1",
+                    # "BA.1.1",
                     "BA.2",
                     # "BA.2.12",
                     "BA.2.12.1",
