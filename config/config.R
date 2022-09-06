be7ebdce3f24dfbea1ff7006c79aaf43503c17c5
Hassell <ncy6@login02.aspen.biotech.cdc.gov>
2022-09-06 11:51:59 -0400

    Production run 2022-09-06 BA.2.7.5 inclusion

--- config/config.R
+++ config/config.R
@@ -42,7 +42,7 @@ data_date <- Sys.Date()
 
 # results folder name inherits from data_date for auto completion, however the set name needs to be edited to 
 # the specified run set before each set is run
-results_folder <- paste0("results_", data_date, "_WOW")
+results_folder <- paste0("results_", data_date, "_BA275")
 
 ## List of variants to track (not just VOC or VOI, but we name them voc in these scripts):
 # These variables (custom_lineage_names, voc*) are *only* used in the weekly_variant_report_nowcast.R script. They are not used in the variant_surveillance_system.R script.
@@ -95,7 +95,7 @@ voc2_additional = c(#"AY.1", "AY.2",
                     "BA.2",
                     "BA.2.12",
                     "BA.2.12.1",
-                    # "BA.2.75",
+                    "BA.2.75",
                     # "BA.3",
                     'BA.4',
                     'BA.4.6',
@@ -272,7 +272,7 @@ use_group_weights <- FALSE
 # "force_aggregate_omicron_except".
 # THIS WILL LIKELY NEED TO BE REPLACED IN THE FUTURE, BUT IT'S HERE TO AVOID
 # SPLITTING OUT BA.1, WHICH IS OFTEN AUTOMATICALLY INCLUDED IN VOC2 B/C IT'S > 1% NATIONALLY.
-force_aggregate_omicron <- TRUE
+force_aggregate_omicron <- FALSE
 # list omicron sublineages that will not be aggregated (if they are also in voc) (these are the only Omicron sublineages that will be permitted)
 force_aggregate_omicron_except <- c('BA.1','BA.2','BA.3','BA.4','BA.5','BA.2.12.1','BA.4.6') # 'BA.2.12', 'BA.1.1'
 
