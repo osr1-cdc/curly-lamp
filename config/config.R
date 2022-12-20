7e738a78ce89a427764e45c3a44b2e06afb1f7bd
Zheng, Xiaoyu (CDC/DDID/NCIRD/OD) (CTR) <qiu5@cdc.gov>
2022-12-20 14:52:41 -0500

    Production Run -2022-12-20-WOW

--- config/config.R
+++ config/config.R
@@ -42,11 +42,11 @@ data_date <- Sys.Date()
 
 # results folder name inherits from data_date for auto completion, however the set name needs to be edited to 
 # the specified run set before each set is run
-results_tag <- "CDT"
+results_tag <- "WOW"
 results_folder <- paste0("results_", data_date, '_', results_tag)
 
 # If pre_aggregation is TRUE, force aggregate sublineages to voc1 list, no need to generate run1 postaggregated nowcast results in run2.
-pre_aggregation <- FALSE
+pre_aggregation <- TRUE
 
 ## List of variants to track (not just VOC or VOI, but we name them voc in these scripts):
 # These variables (custom_lineage_names, voc*) are *only* used in the weekly_variant_report_nowcast.R script. They are not used in the variant_surveillance_system.R script.
@@ -122,6 +122,7 @@ voc2_additional = c(
                     'CK.1',
                     'CR.1.1',
                     'CH.1.1',
+                    'XBB.2',
                     'BA.2.3.20',
                     "B.1.617.2", # Delta
                     "B.1.1.529" # Omicron
@@ -288,7 +289,7 @@ calc_confirmed_infections <- TRUE
 # Option to just fit the nowcast model and avoid the slower parts of the script
 # (this is only valid if the run number == 2)
 # this can be removed eventually. It's here to make it easier to make updates to the Nowcast model.
-nowcast_only = FALSE
+nowcast_only = TRUE
 
 # This is an option that probably won't be used, but I don't want to delete it yet
 # so it's hiding here just in case I want to use it again.
@@ -360,6 +361,9 @@ remove_utahphl <- FALSE
 
 # optionally remove BROAD sequences (b/c they were having trouble with dropout on the Omicron spike protein, resulting in an inability to distinguish between BA.1 and BA.1+R346K in Jan/Feb 2022)
 remove_broad <- FALSE
+
+# optionally remove Quest sequences (b/c there seems to be some XBB sequences from Quest that were not received, so need to make sure the overall proportion is not skewed. Dec. 12, 2022)
+remove_Quest <- FALSE
 
 # new option for testing data exclustion
 exclude_testing_data_portion <- TRUE
