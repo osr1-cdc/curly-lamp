5ffa76459bf347a7bb908944b3607c6c12a7357b
Zheng, Xiaoyu (CDC/DDID/NCIRD/OD) (CTR) <qiu5@cdc.gov>
2023-05-31 15:09:26 -0400

    Ready to merge to master

--- config/config.R
+++ config/config.R
@@ -36,8 +36,8 @@
 # custom_lineages = FALSE
 # set date for data creation
 # (generally set to current date to allow more portability)
-#data_date <- Sys.Date()
-data_date <- as.Date('2023-05-30')
+data_date <- Sys.Date()
+#data_date <- as.Date('2023-05-30')
 # This needs to be a date on which data were frozen in the CDP database
 # Set specific date_frozen to read sequencing data; but read test data and voc list from the data_date backup files. This can be used to rerun modeling using later date backfilled data.
 # Default would be data_date
@@ -50,7 +50,7 @@ use_previously_imported_data <- FALSE
 
 # results folder name inherits from data_date for auto completion, however the set name needs to be edited to
 # the specified run set before each set is run
-results_tag <- "oldagg"
+results_tag <- "CDT_N"
 results_folder <- paste0("results_", data_date, '_', results_tag)
 
 # If pre_aggregation is TRUE, force aggregate sublineages to voc1 list, no need to generate run1 postaggregated nowcast results in run2.
@@ -325,7 +325,7 @@ calc_confirmed_infections <- TRUE
 # Option to just fit the nowcast model and avoid the slower parts of the script
 # (this is only valid if the run number == 2)
 # this can be removed eventually. It's here to make it easier to make updates to the Nowcast model.
-nowcast_only = TRUE
+nowcast_only = FALSE
 
 # This is an option that probably won't be used, but I don't want to delete it yet
 # so it's hiding here just in case I want to use it again.
