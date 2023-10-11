822ba777ab693d481ed99cc4edb9208ad305ccf3
Mittal <ukc2@rosalind01.hpc.biotech.cdc.gov>
2023-10-11 10:44:29 -0400

    Production runs: 2023-10-10

--- config/config.R
+++ config/config.R
@@ -37,7 +37,7 @@
 # set date for data creation
 # (generally set to current date to allow more portability)
 # data_date <- Sys.Date()
-data_date <- as.Date('2023-10-04')
+data_date <- as.Date('2023-10-10')
 # This needs to be a date on which data were frozen in the CDP database
 # Set specific date_frozen to read sequencing data; but read test data and voc list from the data_date backup files. This can be used to rerun modeling using later date backfilled data.
 # Default would be data_date
@@ -101,9 +101,11 @@ voc1 = c(# "AY.1", "AY.2",
          'XBB.1.5.68',
          'XBB.1.5.70',
          'XBB.1.5.72',
+         'GK.1.1',
          'GK.2',
          'HV.1',
          'XBB.1.9.2',
+         'HK.3',
          'EG.5',
          'EG.6.1',
          'XBB.1.16',
@@ -168,9 +170,11 @@ voc2_additional = c(
                     'XBB.1.5.68',
                     'XBB.1.5.70',
                     'XBB.1.5.72',
+                    'GK.1.1',
                     'GK.2',
                     'HV.1',
                     'XBB.1.9.2',
+                    'HK.3',
                     'EG.5',
                     'EG.6.1',
                     'XBB.1.16',
