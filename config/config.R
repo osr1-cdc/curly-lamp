3aa13c1f6ccf16369a1001e4cb53d5513ca34b1e
jcb0 <jcb0@cdc.gov>
2025-01-02 10:47:37 -0500

    Production runs: 2024-12-31

--- config/config.R
+++ config/config.R
@@ -37,7 +37,7 @@
 # set date for data creation
 # (generally set to current date to allow more portability)
 # data_date <- Sys.Date()
-data_date <- as.Date('2024-12-24')
+data_date <- as.Date('2024-12-31')
 # This needs to be a date on which data were frozen in the CDP database
 # Set specific date_frozen to read sequencing data; but read test data and voc list from the data_date backup files. This can be used to rerun modeling using later date backfilled data.
 # Default would be data_date
@@ -165,6 +165,7 @@ voc1 = c(# "AY.1", "AY.2",
          'LP.8.1',
          'MC.19',
          'XEK',
+         'XEC.4',
          'JN.1.18.6') # Omicron
 # define an alternate set of vocs
 # (the reason for including two sets instead of just redefining the first set is
@@ -276,6 +277,7 @@ voc2_additional = c(
                     'LP.8.1',
                     'MC.19',
                     'XEK',
+                    'XEC.4',
                     'JN.1.18.6' # Omicron
                     )
 # voc2_custom = c(voc2,
